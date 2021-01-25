0f93ab5c8666d02b2c3899fb8137576127a11d61
Derek Wang <derek.wang.engineer@gmail.com>
2021-01-25 11:20:32 +0800

    - Create post request for adding new card on server

--- src/server/server.js
+++ src/server/server.js
@@ -17,6 +17,19 @@ app.get('/cards', (req, res) => {
   res.json(cards);
 });
 
+app.post('/cards', (req, res) => {
+  try {
+    cards.push(req.body);
+    return res.status(200).json({
+      message: 'New card is registered successfully.'
+    })
+  } catch (error) {
+    return res.status(400).json({
+      message: 'Failed to register a new card.'
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
